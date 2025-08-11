59f5e7a32df0e9746297de1d08c0b6499b883b01
Pirvu Daniel Catalin <daniel.catalin.pirvu@gmail.com>
2025-08-11 15:57:53 +0300

    Test Branch for iOS Debug

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(cors({
     'http://localhost:5173',
 
     // Cloudflare
+    'https://feature-ios-compatibility.astrolumina.pages.dev',
     'https://astrolumina.pages.dev',
     'https://development.astrolumina.pages.dev',
 
